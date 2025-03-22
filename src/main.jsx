6913606e9c4cab61e09191351ae4d359da9a9bfa
Viveena Khatri <157346644+Viveena@users.noreply.github.com>
2025-03-22 17:57:27 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,9 @@ import {Provider} from 'react-redux'
 import {Toaster } from 'react-hot-toast';
 import './index.css'
 import App from './App.jsx'
+import viteLogo from '/vite.svg';
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
